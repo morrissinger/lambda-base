23b008cb32a6472bf81ad7fb9f57f47885a4ed29
Morris Singer <morrissinger@gmail.com>
2018-03-21 11:09:24 -0400

    allow local npm packages to be included in lambda zip

--- tasks/package.js
+++ tasks/package.js
@@ -5,17 +5,19 @@ import clean from 'gulp-clean';
 import zip from 'gulp-zip';
 import runSequence from 'run-sequence';
 import eventStream from 'event-stream';
-
+import replace from 'gulp-replace';
 
 import {TASKS, STREAMS, INSTALL_COMMAND} from './constants';
 import PATHS from './paths';
 
+
 gulp.task(TASKS.package.build, [TASKS.build.build], (cb) => {
   const stream1 = gulp.src(PATHS.packageJson)
-    .pipe(gulp.dest(PATHS.package))
+      .pipe(replace('file:../', 'file:../../'))
+      .pipe(gulp.dest(PATHS.package));
 
   const stream2 = gulp.src(`${PATHS.lib}/${PATHS.recursive}`)
-    .pipe(gulp.dest(PATHS.package));
+      .pipe(gulp.dest(PATHS.package));
 
   eventStream.merge(stream1, stream2).on(STREAMS.event.end, () => {
     exec(INSTALL_COMMAND, {
@@ -28,19 +30,19 @@ gulp.task(TASKS.package.build, [TASKS.build.build], (cb) => {
 });
 
 gulp.task(TASKS.package.zip, [TASKS.package.build], () =>
-  gulp.src(`${PATHS.package}/${PATHS.recursive}`)
-    .pipe(zip(PATHS.lambdaZip))
-    .pipe(gulp.dest('.'))
+    gulp.src(`${PATHS.package}/${PATHS.recursive}`)
+        .pipe(zip(PATHS.lambdaZip))
+        .pipe(gulp.dest('.'))
 );
 
 gulp.task(TASKS.package.clean, () =>
-  gulp.src(PATHS.package, {read: false})
-    .pipe(clean())
+    gulp.src(PATHS.package, {read: false})
+        .pipe(clean())
 );
 
 gulp.task(TASKS.package.remove, () =>
-  gulp.src(PATHS.lambdaZip, {read: false})
-    .pipe(clean())
+    gulp.src(PATHS.lambdaZip, {read: false})
+        .pipe(clean())
 );
 
 gulp.task(TASKS.package.package, (cb) => runSequence(TASKS.package.zip, TASKS.package.clean, cb));
